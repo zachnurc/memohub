aca9082faa37c120bb795fb0138eaca7f97d0312
zachnurc <zach.nurcombe@gmail.com>
2018-02-08 19:30:05 +0000

    basic scrolling override

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './media/logo.png';
 import headerAnimation from './media/12.jpg';
 import WearableImage from './media/wearable.jpg';
@@ -12,17 +13,47 @@ import './App.css';
 
 class App extends Component {
 
-  // componentDidMount() {
-  //   document.addEventListener('scroll', this.handleScroll);
-  // }
-  //
-  // componentWillUnmount() {
-  //   document.removeEventListener('scroll', this.handleScroll);
-  // }
-  //
-  // handleScroll(event){
-  //   window.scrollBy(0, 10);
-  // }
+  constructor(props) {
+  super(props);
+  this.state = {
+    navCounter: 0
+  };
+}
+
+
+  handleScroll(keycode){
+    var activeNav = [
+      ReactDOM.findDOMNode(this.refs.home),
+      ReactDOM.findDOMNode(this.refs.PanicButton),
+      ReactDOM.findDOMNode(this.refs.IoTSensor),
+      ReactDOM.findDOMNode(this.refs.MemoApp),
+      ReactDOM.findDOMNode(this.refs.MemoHub),
+      ReactDOM.findDOMNode(this.refs.contact)]
+
+    if(keycode == 40){
+      if(this.state.navCounter < 6){
+        this.state.navCounter++;
+      }
+    }
+    else if(keycode == 38){
+      if(this.state.navCounter > 0){
+        this.state.navCounter--;
+      }
+    }
+
+    activeNav[this.state.navCounter].focus();
+  }
+
+  componentDidMount() {
+    //   document.addEventListener('scroll', this.handleScroll);
+    window.addEventListener("keydown", (e) => {
+      // space, page up, page down and arrow keys:
+      if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+        this.handleScroll(e.keyCode);
+      }
+    }, false);
+  }
 
   render() {
     return (
@@ -83,7 +114,7 @@ class App extends Component {
               </div>
             </div>
           </div>
-          <div className="contact">
+          <div id="contact" className="contact">
             <h1>Contact</h1>
           </div>
           <footer className="footer">
@@ -92,8 +123,12 @@ class App extends Component {
           <div id="navbar">
             <ul>
               <li>
-                <a href="#home"><span></span></a>
-                <a href="#PanicButton"><span></span></a>
+                <a ref="home" href="#home"><span></span></a>
+                <a ref="PanicButton" href="#PanicButton"><span></span></a>
+                <a ref="IoTSensor" href="#IoTSensor"><span></span></a>
+                <a ref="MemoApp" href="#MemoApp"><span></span></a>
+                <a ref="MemoHub" href="#MemoHub"><span></span></a>
+                <a ref="contact" href="#contact"><span></span></a>
               </li>
             </ul>
           </div>
