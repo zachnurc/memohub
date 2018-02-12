2e76f3daee4af3d9c0aebff2ab87bcb9eaba8bd6
zachnurc <zach.nurcombe@gmail.com>
2018-02-12 15:49:55 +0000

    scrolling works!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import logo from './media/logo.png';
 import headerAnimation from './media/12.jpg';
 import WearableImage from './media/wearable.jpg';
 import './App.css';
+import Link from './Link.js'
 
 //scroll event listener
 //e.defaultOverride()
@@ -14,35 +14,32 @@ import './App.css';
 class App extends Component {
 
   constructor(props) {
-  super(props);
-  this.state = {
-    navCounter: 0
-  };
-}
-
-
-  handleKeyPress(keycode){
-    var activeNav = [
-      ReactDOM.findDOMNode(this.refs.home),
-      ReactDOM.findDOMNode(this.refs.products1),
-      ReactDOM.findDOMNode(this.refs.products2),
-      ReactDOM.findDOMNode(this.refs.contact)]
-
-    if(keycode == 40){
-      if(this.state.navCounter < 3){
-        this.state.navCounter++;
-      }
-    }
-    else if(keycode == 38){
-      if(this.state.navCounter > 0){
-        this.state.navCounter--;
-      }
-    }
-
-    activeNav[this.state.navCounter].focus();
+    super(props);
+    this.state = {
+      scrollLocation: 0
+    };
   }
 
-  handleScroll(){
+  handleKeyPress(keycode){
+
+    if(keycode === 40){
+      this.handleScroll('down');
+    }
+    else if(keycode === 38){
+      this.handleScroll('up');
+    }
+  }
+
+//instead of scrolling load page with relevant #link
+
+  handleScroll(direction){
+    if(direction === 'down'){
+      window.scroll(0, this.state.scrollLocation + window.innerHeight);
+    } else if(direction === 'up'){
+      window.scroll(0, this.state.scrollLocation - window.innerHeight);
+    }
+
+    this.setState({ scrollLocation: window.scrollY })
 
   }
 
@@ -55,8 +52,13 @@ class App extends Component {
         this.handleKeyPress(e.keyCode);
       }
     }, false);
-    window.addEventListener("scroll", (e) => {
-      this.handleScroll(e.keyCode);
+
+    window.addEventListener("scroll", (event) => {
+      if (window.scrollY > this.state.scrollLocation){
+        this.handleScroll('down');
+      }else if(window.scrollY < this.state.scrollLocation){
+        this.handleScroll('up');
+      }
     }, false);
   }
 
@@ -64,74 +66,82 @@ class App extends Component {
     return (
       <div className="App">
         <div id="main">
-          <header id="home" className="header">
-            <img src={headerAnimation} className="header-video" />
-            <div className="intro">
-              <img className="page-title" src={logo} alt={logo} />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat  pretium vulputate, non metus. Cras aliquet hendrerit lectus. Sed auctor vestibulum feugiat. Pellentesque tempus libero non neque elementum bibendum. Morbi nibh orci, consequat ac hendrerit nec, feugiat eget metus. Etiam malesuada orci eu nunc cursus viverra. Duis eros purus, ornare eu maximus fringilla, lacinia a erat. Cras convallis elementum imperdiet. Nulla facilisi. Aliquam sed venenatis nisl.
-              </p>
-            </div>
-          </header>
-          <div className="products">
-            <div id="products1" className="product">
-              <div className="product-left">
-                <img className="product-image" src={WearableImage} alt="placeholder"/>
-              </div>
-              <div className="product-desc">
-                <h3 className="product-title">Wearable Panic Button</h3>
+          <div id="container">
+            <header id="home" className="header">
+              <img src={headerAnimation} className="header-video" alt="header-img" />
+              <div className="intro">
+                <img className="page-title" src={logo} alt={logo} />
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat  pretium vulputate, non metus. Cras aliquet hendrerit lectus. Sed auctor vestibulum feugiat. Pellentesque tempus libero non neque elementum bibendum. Morbi nibh orci, consequat ac hendrerit nec, feugiat eget metus. Etiam malesuada orci eu nunc cursus viverra. Duis eros purus, ornare eu maximus fringilla, lacinia a erat. Cras convallis elementum imperdiet. Nulla facilisi. Aliquam sed venenatis nisl.
                 </p>
               </div>
+            </header>
+            <div className="products">
+              <div id="products1" className="product">
+                <div className="product-left">
+                  <img className="product-image" src={WearableImage} alt="placeholder"/>
+                </div>
+                <div className="product-desc">
+                  <h3 className="product-title">Wearable Panic Button</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
+                  </p>
+                </div>
+              </div>
+              <div className="product">
+                <div className="product-left">
+                  <img className="product-image" src={WearableImage} alt="placeholder"/>
+                </div>
+                <div className="product-desc">
+                  <h3 className="product-title">IoT Sensor</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
+                  </p>
+                </div>
+              </div>
+              <div id="products2" className="product">
+                <div className="product-desc">
+                  <h3 className="product-title">Memo App</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
+                  </p>
+                </div>
+                <div className="product-right">
+                  <img className="product-image" src={WearableImage} alt="placeholder"/>
+                </div>
+              </div>
+              <div className="product">
+                <div className="product-desc">
+                  <h3 className="product-title">Memo Hub</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
+                  </p>
+                </div>
+                <div className="product-right">
+                  <img className="product-image" src={WearableImage} alt="placeholder"/>
+                </div>
+              </div>
             </div>
-            <div className="product">
-              <div className="product-left">
-                <img className="product-image" src={WearableImage} alt="placeholder"/>
-              </div>
-              <div className="product-desc">
-                <h3 className="product-title">IoT Sensor</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
-                </p>
-              </div>
+            <div id="contact" className="contact">
+              <h1>Contact</h1>
             </div>
-            <div id="products2" className="product">
-              <div className="product-desc">
-                <h3 className="product-title">Memo App</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
-                </p>
-              </div>
-              <div className="product-right">
-                <img className="product-image" src={WearableImage} alt="placeholder"/>
-              </div>
-            </div>
-            <div className="product">
-              <div className="product-desc">
-                <h3 className="product-title">Memo Hub</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et condimentum est. Sed sit amet convallis metus. Nam vel cursus justo. Curabitur dictum sollicitudin nulla non rhoncus. Nulla finibus hendrerit tellus vitae consequat. Suspendisse imperdiet magna enim. Cras eget mi metus. Ut facilisis vestibulum sem eu iaculis. Donec nec lorem velit. Aliquam magna sapien, rutrum et risus vitae, aliquam venenatis ex. Ut sit amet blandit urna. Fusce sagittis tellus eu ipsum volutpat pretium vulputate non metus.
-                </p>
-              </div>
-              <div className="product-right">
-                <img className="product-image" src={WearableImage} alt="placeholder"/>
-              </div>
-            </div>
+            <footer className="footer">
+              <h3>Copyright</h3>
+            </footer>
           </div>
-          <div id="contact" className="contact">
-            <h1>Contact</h1>
-          </div>
-          <footer className="footer">
-            <h3>Copyright</h3>
-          </footer>
           <div id="navbar">
             <ul>
               <li>
-                <a ref="home" href="#home"><span></span></a>
-                <a ref="products1" href="#products1"><span></span></a>
-                <a ref="products2" href="#products2"><span></span></a>
-                <a ref="contact" href="#contact"><span></span></a>
+                <Link ref='home' href='#home' class="active"/>
+              </li>
+              <li>
+                <Link ref='products1' href='#products1'/>
+              </li>
+              <li>
+                <Link ref='products2' href='#products2'/>
+              </li>
+              <li>
+                <Link ref='contact' href='#contact'/>
               </li>
             </ul>
           </div>
